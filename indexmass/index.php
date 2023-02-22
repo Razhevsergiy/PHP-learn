f52200bcd6fcf6bf32e7a4ca6d1bf09efe97c0f7
Sergiy <razhevsergiy@gmail.com>
2023-02-22 21:21:36 +0300

    .

--- indexmass/index.php
+++ indexmass/index.php
@@ -7,21 +7,24 @@
     <body>
         <?php
         require_once"/home/serg/PHP/dumper.php";
-        if (isset($_REQUEST['doGo'])) {
-            //$indexmass = ;
-            echo "<p>Ваш индекс массы тела равен ".
-            $_REQUEST["mass"]/(($_REQUEST["height"]/100)**2)." .";
+        
+        if ((isset($_REQUEST['doGo']))) {
+            
+            $indexmass = $_REQUEST["var"]["mass"]/
+            (($_REQUEST["var"]["height"]/100)**2);
+            echo "<p>Ваш индекс массы тела равен: ".$indexmass
+            ." .";
         }
 
         
         //dumper($GLOBALS);
         ?>
-        <form action=<?=$_SERVER['SCRIPT_NAME']?> method="get">
+        <form action=<?=$_SERVER['SCRIPT_NAME']?> method="post">
         <label for="height">Введите рост в сантиметрах:
-            <input type="text" name="height" value="1" id="height">
+            <input type="text" name="var[height]" value="1" id="height">
         </input></label>
         <label for="mass">Введите вес в килограммах:
-            <input type="text" name="mass" value="1" id="mass">
+            <input type="text" name="var[mass]" value="1" id="mass">
         </input></label>
         <input type="submit" value="Рассчитать" name="doGo">
         </form>
